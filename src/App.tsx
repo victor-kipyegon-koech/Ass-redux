eb774fda83f31685cb20ce4f0e03b25fb4b739a3
victor-kipyegon-koech <victorkipyegon778@gmail.com>
2025-06-16 16:54:16 +0300

    error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
- import { useState } from 'react';
+ 
 import { Provider } from 'react-redux';
 import TodoComponent from './components/TodoComponent'
 import { store } from './app/store'; 
@@ -6,7 +6,7 @@ import Counter from './components/counter';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <Provider store={store}>
